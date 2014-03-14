449b9a77d3c9029a3f2cdaad269a8f60fe511aeb
John Knott <john.knott@gmail.com>
2014-03-14 15:23:28 +0000

    Emptied repository

--- lib/countries/version.rb
+++
@@ -1,3 +0,0 @@
-module Countries
-  VERSION = "0.8.3"
-end
--- lib/iso3166.rb
+++
@@ -1,6 +0,0 @@
-require 'yaml'
-require 'iso4217'
-
-require 'countries/select_helper'
-require 'countries/telephone_prefix_select_helper'
-require 'countries/country'
